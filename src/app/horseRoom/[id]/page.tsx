4adc9855b811de8745ee1299be2b6e9004a348f2
Brayan Medrano <brayan@Brayans-MacBook-Air.local>
2025-11-29 17:03:13 -0800

    Removed extra color class

--- src/app/horseRoom/[id]/page.tsx
+++ src/app/horseRoom/[id]/page.tsx
@@ -76,22 +76,6 @@ export default function HorseRoomPage() {
       }
     }
   }, []);
-
-  // ---- Scroll chat to bottom on new messages ----
-  const COLOR_CLASSES = [
-    "bg-blue-600",
-    "bg-rose-600",
-    "bg-emerald-600",
-    "bg-amber-600",
-    "bg-fuchsia-600",
-    "bg-cyan-600",
-    "bg-indigo-600",
-    "bg-lime-600",
-    "bg-teal-600",
-    "bg-orange-600",
-    "bg-pink-600",
-    "bg-purple-600",
-  ];
   
   useEffect(() => {
     const loadRace = async () => {
